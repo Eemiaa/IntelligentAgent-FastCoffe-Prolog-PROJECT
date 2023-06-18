aa112378f35ea98d033ba57aa5c283de94494c6b
jordany <jordanyluiz@gmail.com>
2023-06-18 12:05:56 -0300

    feat: adding notification rules

--- orderFlowController.pl
+++ orderFlowController.pl
@@ -1,45 +1,93 @@
 :- dynamic pedido/2.
+:- dynamic notificacaoAgendada/3.
 
-item(1, 3.0, 5).
-item(2, 2.0, 4).
-item(3, 4.0, 8).
-item(4, 3.0, 4).
-item(5, 5.0, 8).
-item(6, 6.50, 5).
-item(7, 8.0, 9).
+/* =============================== ITENS DO CARDÁPIO =============================== */
+
+item(1, 3.0, 1).
+item(2, 2.0, 2).
+item(3, 4.0, 3).
+item(4, 3.0, 1).
+item(5, 5.0, 2).
+item(6, 6.50, 2).
+item(7, 8.0, 3).
+
+/* =========================== REGRAS E FATOS AUXILIARES =========================== */
+
+obterNumeroDoItemEQuantidade([I, Q | _], I, Q). 
+
 
 criarIDPedido(ID):- random_between(1, 10, Numero),
                     not(pedido(Numero,_)),
                     ID = Numero, !.
 
-criarIDPedido(ID):- write('Já tem'), 
-                    criarIDPedido(ID).
-        
-cardapio :- write('1. Americano................R$3,00'), nl,
-            write('2. Cappuccino...............R$2,00'), nl,
-            write('3. Double Expresso..........R$4,00'), nl,
-            write('4. Latte....................R$3,00'), nl,
-            write('5. Macchiato................R$5,00'), nl,
-            write('6. Mint chocolate...........R$6,50'), nl,
-            write('7. Expresso.................R$8,00').
+criarIDPedido(ID):- criarIDPedido(ID).
 
-obterNumeroDoItemEQuantidade([I, Q | _], I, Q). 
 
 calcularPrecoEEsperaTotal([], PTotal, ETotal, PTotal, ETotal).
 
-calcularPrecoEEsperaTotal([ItemN|Restante], PTotalAux, ETotalAux, PTotal, ETotal) :-  obterNumeroDoItemEQuantidade(ItemN, NumeroItem, Quantidade),
-                                                                                      item(NumeroItem, Preco, Espera),
-                                                                                      AuxP is PTotalAux + Preco * Quantidade,
-                                                                                      AuxE is ETotalAux + Espera * Quantidade,
-                                                                                      calcularPrecoEEsperaTotal(Restante, AuxP, AuxE, PTotal, ETotal), !.
-
+calcularPrecoEEsperaTotal([ItemN|Restante], PTotalAux, ETotalAux, PTotal, ETotal):- obterNumeroDoItemEQuantidade(ItemN, NumeroItem, Quantidade),
+                                                                                    item(NumeroItem, Preco, Espera),
+                                                                                    AuxP is PTotalAux + Preco * Quantidade,
+                                                                                    AuxE is ETotalAux + Espera * Quantidade,
+                                                                                    calcularPrecoEEsperaTotal(Restante, AuxP, AuxE, PTotal, ETotal), !.
 
 calcularPrecoEEsperaTotal(Itens, PTotal, ETotal) :- calcularPrecoEEsperaTotal(Itens, 0, 0, PTotal, ETotal).
+
+
+obterTempoAtualEmSegundos(TempoSegundos) :-
+    get_time(Tempo),
+    floor(Tempo, TempoSegundos).
+
+
+lerPid(PID) :- open('pid.txt', read, Stream),
+                read_line_to_string(Stream, PIDString),
+                close(Stream),
+                number_string(PID, PIDString).
+
+
+agendarNotificacao(Segundos, ID) :-
+    format(string(Comando), "sleep ~w && DISPLAY=:0 notify-send 'O pedido ~w está pronto' & echo $! > pid.txt", [Segundos, ID]),
+    shell(Comando),
+    lerPid(PID),
+    obterTempoAtualEmSegundos(TempoAtual),
+    write('tempo atual = '), write(TempoAtual), nl,
+    TempoPrevisto is TempoAtual + Segundos,
+    assertz(notificacaoAgendada(PID, ID, TempoPrevisto)).
+
+
+cancelarNotificacao(ID) :-
+    retract(notificacaoAgendada(PID, ID, _)),
+    format(string(Comando), 'kill ~w', [PID]),
+    shell(Comando).
+
+
+adiarNotificacao(MaisSegundos, ID) :-
+    notificacaoAgendada(_, ID, TempoPrevisto),
+    obterTempoAtualEmSegundos(TempoAtual),
+    TempoDecorrido is TempoPrevisto - TempoAtual,
+    write('Quanto ta faltando pra finalizar o pedido '), write(ID), write('= '), write(TempoDecorrido), nl,
+    NovosSegundos is TempoDecorrido + MaisSegundos,
+    cancelarNotificacao(ID),
+    agendarNotificacao(NovosSegundos, ID).
+
+/* =========================== REGRAS USADAS PELO CLIENTE ========================== */
+
+cardapio :- write('+------------------------------------+'), nl,
+            write('|              CARDAPIO              |'), nl,
+            write('+------------------------------------+'), nl,
+            write('| 1. Americano................R$3,00 |'), nl,
+            write('| 2. Cappuccino...............R$2,00 |'), nl,
+            write('| 3. Double Expresso..........R$4,00 |'), nl,
+            write('| 4. Latte....................R$3,00 |'), nl,
+            write('| 5. Macchiato................R$5,00 |'), nl,
+            write('| 6. Mint chocolate...........R$6,50 |'), nl,
+            write('| 7. Expresso.................R$8,00 |'), nl,
+            write('+------------------------------------+').
                                                                 
 fazerPedido(Espera, ID) :-  findall(pedido(X,Y), pedido(X,Y), Pedidos),
                             write(Pedidos), nl,
                             length(Pedidos, Tamanho),
-                            Tamanho =< 10,
+                            Tamanho < 10,
                             write('Insira seu pedido em uma lista de listas: '), nl,
                             write('[[< Numero do item >, < Quantidade >], ...]'), nl,
                             read(Itens), nl, 
@@ -47,6 +95,4 @@ fazerPedido(Espera, ID) :-  findall(pedido(X,Y), pedido(X,Y), Pedidos),
                             criarIDPedido(ID),
                             asserta(pedido(ID, PTotal)), !.
 
-fazerPedido(_, _) :- write('Temos muitos pedidos em andamento, volte mais tarde...').
-                            
-                            
+fazerPedido(_, _) :- write('Temos muitos pedidos em andamento, volte mais tarde...').                            
