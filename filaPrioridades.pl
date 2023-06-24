d182b8c28b4e6dae2104169dc1b9e20d3861fb33
jordany <jordanyluiz@gmail.com>
2023-06-24 15:21:01 -0300

    feat: finishing alterar_prioridade

--- filaPrioridades.pl
+++ filaPrioridades.pl
@@ -4,12 +4,12 @@ fila_prioridades([]).
 cont(0).
 
 % Adicionar um pedido à fila de prioridades
-adicionar_pedido(ID, Preco, Prioridade) :-
+adicionar_pedido(ID, Preco, Prioridade, Descricao) :-
     retract(cont(C)),
     NewC is C + 1,
     asserta(cont(NewC)),
     retract(fila_prioridades(Fila)),
-    inserir_pedido(Fila, pedido(ID, Preco, Prioridade, C), NovaFila),
+    inserir_pedido(Fila, pedido(ID, Preco, Prioridade, C, Descricao), NovaFila),
     assertz(fila_prioridades(NovaFila)).
 
 % Remover um pedido da fila de prioridades
@@ -22,7 +22,9 @@ remover_pedido(ID) :-
 alterar_prioridade(ID, NovaPrioridade) :-
     retract(fila_prioridades(Fila)),
     alterar_prioridade(Fila, ID, NovaPrioridade, NovaFila),
-    assertz(fila_prioridades(NovaFila)).
+    write(NovaFila),
+    reordenar_fila_prioridades(NovaFila, FilaOrdenada),
+    assertz(fila_prioridades(FilaOrdenada)).
 
 % Inserir um pedido na fila de prioridades
 inserir_pedido([], Pedido, [Pedido]).
@@ -34,20 +36,26 @@ inserir_pedido([Pedido1|Resto], Pedido2, [Pedido1|NovaFila]) :-
 % Remover um pedido da fila de prioridades
 remover_pedido([], _, []).
 remover_pedido([Pedido|Resto], ID, NovaFila) :-
-    Pedido = pedido(ID, _, _, _),
+    Pedido = pedido(ID, _, _, _, _),
     remover_pedido(Resto, ID, NovaFila).
 remover_pedido([Pedido|Resto], ID, [Pedido|NovaFila]) :-
     remover_pedido(Resto, ID, NovaFila).
 
 % Alterar a prioridade de um pedido na fila de prioridades
 alterar_prioridade([], _, _, []).
-alterar_prioridade([pedido(ID, Preco, _)|Resto], ID, NovaPrioridade, [pedido(ID, Preco, NovaPrioridade)|NovaFila]) :-
-    !,
-    alterar_prioridade(Resto, ID, NovaPrioridade, NovaFila).
+alterar_prioridade([pedido(ID, Preco, _, C, Descricao)|Resto], ID, NovaPrioridade, [pedido(ID, Preco, NovaPrioridade, C, Descricao)|NovaFila]) :-
+    write('foi'), nl, 
+    alterar_prioridade(Resto, ID, NovaPrioridade, NovaFila), !.
 alterar_prioridade([Pedido|Resto], ID, NovaPrioridade, [Pedido|NovaFila]) :-
     alterar_prioridade(Resto, ID, NovaPrioridade, NovaFila).
 
 % Comparar as prioridades entre dois pedidos
-compara_prioridades(pedido(_, _, true, _), pedido(_, _, false, _)).
-compara_prioridades(pedido(_, _, true, C1), pedido(_, _, true, C2)) :-
+compara_prioridades(pedido(_, _, true, _, _), pedido(_, _, false, _, _)).
+compara_prioridades(pedido(_, _, true, C1, _), pedido(_, _, true, C2, _)) :-
     C1 =< C2.
+
+% Reordenar a fila de prioridades com base nas novas prioridades
+reordenar_fila_prioridades([], []).
+reordenar_fila_prioridades([Pedido|Resto], FilaOrdenada) :-
+    reordenar_fila_prioridades(Resto, FilaOrdenadaResto),
+    inserir_pedido(FilaOrdenadaResto, Pedido, FilaOrdenada).
--- orderFlowController.pl
+++ orderFlowController.pl
@@ -1,40 +1,63 @@
-:- consult('filaPrioridades.pl').
-
-:- dynamic pedido/2.
+:- consult('teste.pl').
+:- dynamic pedido/5.
 :- dynamic notificacaoAgendada/3.
 
-/* =============================== ITENS DO CARDÁPIO =============================== */
+/* =============================== ITENS DO CARDAPIO =============================== */
 
-item(1, 3.0, 60).
-item(2, 2.0, 120).
-item(3, 4.0, 180).
-item(4, 3.0, 60).
-item(5, 5.0, 120).
-item(6, 6.50, 120).
-item(7, 8.0, 180).
+item(1, 3.0, 1, 'Americano').
+item(2, 2.0, 2, 'Cappuccino').
+item(3, 4.0, 3, 'Double Expresso').
+item(4, 3.0, 1, 'Latte').
+item(5, 5.0, 2, 'Macchiato').
+item(6, 6.50, 2, 'Mint chocolate').
+item(7, 8.0, 3, 'Expresso').
 
 /* =========================== REGRAS E FATOS AUXILIARES =========================== */
+
+/* *************************** Gerencia de Pedidos ********************************* */
+
+criarMensagem([], ''). 
+criarMensagem([ Item ], Mensagem) :- obterNumeroDoItemEQuantidade(Item, I, Q),
+                                     item(I, _, _, Nome),
+                                     atomic_list_concat([Q, Nome], ' ', Mensagem).
+criarMensagem([ Item | Resto ], Mensagem) :- 
+    obterNumeroDoItemEQuantidade(Item, I, Q),
+    item(I, _, _, Nome),
+    criarMensagem(Resto, MensagemResto),
+    atomic_list_concat([Q, ' ', Nome, ', ', MensagemResto], Mensagem).
+
+
+buscarPedidoPeloID([pedido(ID, Preco, _, _, Itens) | _], ID, Preco, Itens) :- !.
+buscarPedidoPeloID([pedido(_, _, _, _, _) | Resto], ID, Preco, Itens) :- buscarPedidoPeloID(Resto, ID, Preco, Itens).
+
 
 obterNumeroDoItemEQuantidade([I, Q | _], I, Q). 
 
 
-criarIDPedido(ID):- random_between(1, 10, Numero),
-                    not(pedido(Numero,_)),
-                    ID = Numero, !.
+criarIDPedido(ID):- 
+    random_between(1, 10, Numero),
+    fila_prioridades(Fila),
+    not(buscarPedidoPeloID(Fila, Numero, _, _)),
+    ID = Numero, !.
+
 
 criarIDPedido(ID):- criarIDPedido(ID).
 
 
 calcularPrecoEEsperaTotal([], PTotal, ETotal, PTotal, ETotal).
 
-calcularPrecoEEsperaTotal([ItemN|Restante], PTotalAux, ETotalAux, PTotal, ETotal):- obterNumeroDoItemEQuantidade(ItemN, NumeroItem, Quantidade),
-                                                                                    item(NumeroItem, Preco, Espera),
-                                                                                    AuxP is PTotalAux + Preco * Quantidade,
-                                                                                    AuxE is ETotalAux + Espera * Quantidade,
-                                                                                    calcularPrecoEEsperaTotal(Restante, AuxP, AuxE, PTotal, ETotal), !.
+
+calcularPrecoEEsperaTotal([ItemN|Restante], PTotalAux, ETotalAux, PTotal, ETotal):- 
+    obterNumeroDoItemEQuantidade(ItemN, NumeroItem, Quantidade),
+    item(NumeroItem, Preco, Espera, _),
+    AuxP is PTotalAux + Preco * Quantidade,
+    AuxE is ETotalAux + Espera * Quantidade,
+    calcularPrecoEEsperaTotal(Restante, AuxP, AuxE, PTotal, ETotal), !.
+
 
 calcularPrecoEEsperaTotal(Itens, PTotal, ETotal) :- calcularPrecoEEsperaTotal(Itens, 0, 0, PTotal, ETotal).
 
+/* ************************* Gerencia de Notificacoes ****************************** */
 
 obterTempoAtualEmSegundos(TempoSegundos) :-
     get_time(Tempo),
@@ -48,7 +71,10 @@ lerPid(PID) :- open('pid.txt', read, Stream),
 
 
 agendarNotificacao(Segundos, ID) :-
-    format(string(Comando), "sleep ~w && DISPLAY=:0 notify-send 'O pedido ~w está pronto' & echo $! > pid.txt", [Segundos, ID]),
+    fila_prioridades(Fila),
+    buscarPedidoPeloID(Fila, ID, _, Itens),
+    criarMensagem(Itens, Mensagem),
+    format(string(Comando), "sleep ~w && echo '\nO pedido ~w está pronto: \n ~w' > /dev/pts/1 & echo $! > pid.txt", [Segundos, ID, Mensagem]),
     shell(Comando),
     lerPid(PID),
     obterTempoAtualEmSegundos(TempoAtual),
@@ -74,32 +100,30 @@ adiarNotificacao(MaisSegundos, ID) :-
 
 /* =========================== REGRAS USADAS PELO CLIENTE ========================== */
 
-cardapio :- write('+------------------------------------+'), nl,
-            write('|              CARDAPIO              |'), nl,
-            write('+------------------------------------+'), nl,
-            write('| 1. Americano................R$3,00 |'), nl,
-            write('| 2. Cappuccino...............R$2,00 |'), nl,
-            write('| 3. Double Expresso..........R$4,00 |'), nl,
-            write('| 4. Latte....................R$3,00 |'), nl,
-            write('| 5. Macchiato................R$5,00 |'), nl,
-            write('| 6. Mint chocolate...........R$6,50 |'), nl,
-            write('| 7. Expresso.................R$8,00 |'), nl,
-            write('+------------------------------------+').
+cardapio :- findall(item(Numero, Preco, _, Descricao), item(Numero, Preco, _, Descricao), Itens),
+            write('\u001b[45m =================== \u2615 CARDAPIO \u2615 ====================\u001b[m'), nl,
+            mostrarCardapio(Itens).
+
+mostrarCardapio([]) :-  write('\u001b[45m =======================================================\u001b[m'), nl, !.
+mostrarCardapio([item(Numero, Preco, _, Descricao) | Resto]) :- 
+    format('~w. ~w....................~w', [Numero, Descricao, Preco]), nl,
+    mostrarCardapio(Resto).
+
                                                                 
-fazerPedido(Espera, ID) :-  findall(pedido(X,Y), pedido(X,Y), Pedidos),
-                            write(Pedidos), nl,
-                            length(Pedidos, Tamanho),
-                            Tamanho < 10,
-                            write('Insira seu pedido em uma lista de listas: '), nl,
-                            write('[[< Numero do item >, < Quantidade >], ...]'), nl,
-                            read(Itens), nl, 
-                            write('Prioridade? [true/false]: '), nl,
-                            read(Prioridade), nl, 
-                            calcularPrecoEEsperaTotal(Itens, PTotal, Espera),
-                            criarIDPedido(ID),
-                            adicionar_pedido(ID, PTotal, Prioridade),
-                            % verificarEntrada(ID),
-                            agendarNotificacao(Espera, ID), !.
-                            % asserta(pedido(ID, PTotal)), !.
+fazerPedido(Espera, ID) :-  
+    fila_prioridades(Fila),
+    length(Fila, Tamanho),
+    Tamanho < 10,
+    write('Insira seu pedido em uma lista de listas: '), nl,
+    write('[[< Numero do item >, < Quantidade >], ...]'), nl,
+    read(Itens), nl, 
+    write('Prioridade [true/false]: '),nl,
+    read(Prioridade), nl,
+    calcularPrecoEEsperaTotal(Itens, PTotal, Espera),
+    criarIDPedido(ID),
+    adicionar_pedido(ID, PTotal, Prioridade, Itens),
+    agendarNotificacao(Espera, ID),
+    % asserta(pedido(ID, PTotal)), 
+    !.
 
 fazerPedido(_, _) :- write('Temos muitos pedidos em andamento, volte mais tarde...').                            
